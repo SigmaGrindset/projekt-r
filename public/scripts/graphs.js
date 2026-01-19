19f2c4334b495fd106f6e9b3bb87e5be5a01762b
Petar-Gavric <petar.gavric@fer.unizg.hr>
2026-01-19 17:16:40 +0100

    grafovi

--- public/scripts/graphs.js
+++ public/scripts/graphs.js
@@ -5,11 +5,12 @@ let graph1Initialized = false;
 let graph2Initialized = false;
 let graph3Initialized = false;
 let graph4Initialized = false;
+let graph5Initialized = false;
 
 // -------------------------------
 // Graf 1: Vrijeme učenja po predmetu
 // -------------------------------
-async function drawGraphBySubject(timeframe = "day") {
+async function drawGraphBySubject(timeframe) {
   try {
     const res = await fetch("/graph/first", {
       method: "POST",
@@ -18,10 +19,23 @@ async function drawGraphBySubject(timeframe = "day") {
     });
 
     const data = await res.json();
+    let time = "";
+    switch (timeframe) {
+      case "day":
+        time = "(zadnjih 24h)"
+        break;
+      case "week":
+        time = "(zadnjih tjedan dana)"
+        break
+      case "month":
+        time = "(zadnjih mjesec dana)"
+      default:
+        break;
+    }
 
     const layout = {
       title: {
-        text: "Učenje kroz vrijeme",
+        text: "Učenje po predmetu " + time,
         font: { size: 18 },
       },
       autosize: true,
@@ -52,7 +66,7 @@ async function drawGraphBySubject(timeframe = "day") {
 // -------------------------------
 // Graf 2: Učenje kroz vrijeme
 // -------------------------------
-async function drawStudyOverTime(timeframe = "week") {
+async function drawStudyOverTime(timeframe) {
   try {
     const res = await fetch("/graph/second", {
       method: "POST",
@@ -62,13 +76,32 @@ async function drawStudyOverTime(timeframe = "week") {
 
     const data = await res.json();
 
+    // ---------- PRELOM IMENA PREDMETA ZA LEGENDU ----------
+    data[timeframe].forEach(trace => {
+      trace.name = trace.name.replace(/ /g, "<br>");
+    });
+
+    let time = "";
+    switch (timeframe) {
+      case "day":
+        time = "(zadnjih 24h)"
+        break;
+      case "week":
+        time = "(zadnjih tjedan dana)"
+        break
+      case "month":
+        time = "(zadnjih mjesec dana)"
+      default:
+        break;
+    }
+
     const layout = {
       title: {
-        text: "Učenje kroz vrijeme",
+        text: "Učenje kroz vrijeme " + time,
         font: { size: 18 },
       },
       autosize: true,
-      margin: { t: 50, l: 50, r: 20, b: 80 },
+      margin: { t: 50, l: 50, r: 240, b: 140 },
       xaxis: {
         title: "Datum",
         tickangle: -45,
@@ -80,6 +113,14 @@ async function drawStudyOverTime(timeframe = "week") {
       },
       paper_bgcolor: "rgba(0,0,0,0)",
       plot_bgcolor: "rgba(0,0,0,0)",
+      legend: {
+        x: 1,
+        y: 1,
+        xanchor: "left",
+        yanchor: "top",
+        font: { size: 12 },
+        itemwidth: 70  
+      }
     };
 
     if (!graph2Initialized) {
@@ -92,6 +133,7 @@ async function drawStudyOverTime(timeframe = "week") {
     console.error(err);
   }
 }
+
 
 // -------------------------------
 // Graf 3: Učenje po danima u tjednu
@@ -177,6 +219,45 @@ async function drawStudyByHours() {
   }
 }
 
+function getHeatmapClass(minutes) {
+  if (minutes === 0) return "heat-day";
+  if (minutes <= 30) return "heat-day lvl1";
+  if (minutes <= 60) return "heat-day lvl2";
+  if (minutes <= 120) return "heat-day lvl3";
+  if (minutes <= 240) return "heat-day lvl4";
+  if (minutes <= 360) return "heat-day lvl5";
+  return "heat-day lvl6";
+}
+
+async function drawGithubActivity() {
+  try {
+    const res = await fetch("/graph/fifth", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+
+    const container = document.getElementById("graph5");
+    if (!container) return;
+
+    container.innerHTML = "";
+
+    data.reverse();
+
+    for (const day of data) {
+      const div = document.createElement("div");
+      div.className = getHeatmapClass(day.minutes);
+      div.title = `${day.date}: ${day.minutes} min`;
+
+      container.appendChild(div);
+    }
+
+    graph5Initialized = true;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 // -------------------------------
 // Timeframe select (day / week / month)
@@ -190,15 +271,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     drawGraphBySubject(timeframe);
 
-    //if (timeframe === "day") {
-    //  document.getElementById("graph2").style.display = "none";
-    //} else {
-    //  document.getElementById("graph2").style.display = "block";
+    if (timeframe === "day") {
+      document.getElementById("graph2").style.display = "none";
+    } else {
+      document.getElementById("graph2").style.display = "block";
       drawStudyOverTime(timeframe);
-    //}
+    }
 
     drawStudyByDaysOfWeek();
     drawStudyByHours();
+    drawGithubActivity();
   }
 
   // inicijalni prikaz
