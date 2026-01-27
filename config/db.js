5937bef121b26924a3b97f581a8fab5205b5ac07
Antonio Batarilović <antoniobnoni@gmail.com>
2026-01-27 19:11:11 +0100

    neon db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,25 @@
 const { Pool } = require('pg');
-const pool = new Pool({
-  host: "127.0.0.1",
-  port: 5432,
-  user: "postgres",
-  password: "1234",
-  database: "gsadmin"
-});
+
+let pool;
+if (!process.env.NEON_DB_CONNECTION_STRING) {
+  // lokalno sa dockerom
+  pool = new Pool({
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "1234",
+    database: "gsadmin"
+  });
+} else {
+
+  // neon postgres
+  pool = new Pool({
+    connectionString: process.env.NEON_DB_CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+}
 
 
 async function testConnection(req, res) {
